f71259e4f8b5c3bd51e68ad4207fd232bdde2a2a
lukwiya23 <luk23bonnie8@gmail.com>
2019-09-19 00:41:41 +0300

    .

--- attack.py
+++ attack.py
@@ -7,3 +7,7 @@ enemyAttackHigh = 80
 while playerHP > 0:
     damage = random.randrange(enemyAttackLow, enemyAttackHigh)
     playerHP = playerHP-damage
+
+    if(playerHP <= 0):
+        playerHP = 0
+        print("You are dead")
