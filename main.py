8ff1c02018d25a2657dcabed794fbb1a883712b5
lukwiya23 <luk23bonnie8@gmail.com>
2019-09-18 22:39:40 +0300

    if else

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def do_math(num1, num2):
-    return num1 + num2
+check = False
 
-math1 = do_math(5, 7)
-math2 = do_math(12,7)
-
-print("First sum is ", math1, "and the second sum is ", math2)
+if check == False:
+    print("This shit false!")
