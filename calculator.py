7246c5abc7692cc7f53160ba7c0f9d221537f490
lukwiya23 <luk23bonnie8@gmail.com>
2019-09-19 00:17:41 +0300

    creating a calculator

--- calculator.py
+++ calculator.py
@@ -3,3 +3,14 @@ import re
 run = True
 previous = 0
 
+
+def perfom_math():
+    global run
+    global previous
+    equation = input("Enter your equation")
+    if(equation == 'quit'):
+        print("Byeee")
+        run = False
+    else:
+        answer = eval(equation)
+        print(answer)
