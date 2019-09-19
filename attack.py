5ed40b522a9c12c1c830533f459269c519004fa7
lukwiya23 <luk23bonnie8@gmail.com>
2019-09-19 14:51:45 +0300

    continuing the rpg game

--- attack.py
+++ attack.py
@@ -1,9 +1,19 @@
 import random
 
+
+class Enemy:
+    attackLow = 60
+    attackHigh = 80
+
+    def getAttack(self):
+        print(self.attackHigh)
+
+enemy1 = Enemy()
+enemy1.getAttack()
+'''
 playerHP = 5000
 enemyAttackLow = 60
 enemyAttackHigh = 80
-
 while playerHP > 0:
     damage = random.randrange(enemyAttackLow, enemyAttackHigh)
     playerHP = playerHP-damage
@@ -15,3 +25,4 @@ while playerHP > 0:
 
     print("Enemy strikes for", damage, "points of damage")
     print("Current HP is ", playerHP)
+'''
